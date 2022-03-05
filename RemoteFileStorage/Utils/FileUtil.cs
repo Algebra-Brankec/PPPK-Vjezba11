3baa479a11647ea6b06de8e1bdfccc55a8f3bf4b
Branimir <33580274+Brankec@users.noreply.github.com>
2022-03-05 23:18:50 +0100

    Add project files.

---
+++ RemoteFileStorage/Utils/FileUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoteFileStorage.Utils
+{
+    public static class FileUtil
+    {
+        public static List<string> extensionWhitelist = new List<string>() { "jpg", "jpeg", "png", "svg", "tiff", "gif" };
+
+        public static bool IsValidExtension(string extension)
+        {
+            return extensionWhitelist.Contains(extension.ToLower());
+        }
+    }
+}
